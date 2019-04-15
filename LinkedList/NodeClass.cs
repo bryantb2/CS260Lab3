2ec7021893d3c424dc729a492db2856d9b101a64
bryantb2 <bryantb2@my.lanecc.edu>
2019-04-15 12:04:59 -0700

    NodeClass generated

---
+++ LinkedList/NodeClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class NodeClass
+    {
+        //Class Fields
+        private char value;
+        private NodeClass next = null;
+
+        //Defined and Default Constructor
+        public NodeClass(char v, NodeClass next)
+        {
+            this.value = v;
+            this.next = next; //sets the value of the next to the other links
+        }
+
+        //Properties
+        public char Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public NodeClass Next
+        {
+            get
+            {
+                return this.next;
+            }
+            set
+            {
+                this.next = value;
+            }
+        }
+    }
+}
